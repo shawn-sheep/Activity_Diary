d854f4a17e14ab7ca808e1904bdeb25de8c4f39a
AuroraIridescent <2064672878@qq.com>
2023-12-16 22:35:57 +0800

    UI1.0

---
+++ app/src/main/java/de/rampro/activitydiary/ui/generic/AchievementActivity.java
@@ -0,0 +1,70 @@
+/*
+ * ActivityDiary
+ *
+ * Copyright (C) 2023 Raphael Mack http://www.raphael-mack.de
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package de.rampro.activitydiary.ui.generic;
+
+import android.os.Build;
+import android.os.Bundle;
+import androidx.appcompat.app.ActionBar;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+
+import de.rampro.activitydiary.R;
+/*
+ * ActivityDiary
+ *
+ * Copyright (C) 2018 Raphael Mack http://www.raphael-mack.de
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+public class AchievementActivity extends BaseActivity {
+
+    @Override
+    @SuppressWarnings("deprecation")
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_achievement);
+
+
+
+
+
+        //mDrawerToggle.setDrawerIndicatorEnabled(false);
+    }
+
+    @Override
+    public void onResume(){
+        mNavigationView.getMenu().findItem(R.id.nav_achievement).setChecked(true);
+        super.onResume();
+    }
+}
